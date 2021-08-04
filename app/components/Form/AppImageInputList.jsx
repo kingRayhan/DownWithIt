9d3f56b2a3756ad672260efe56bc3af563184079
kingrayhan <rayhan095@gmail.com>
2021-08-04 06:32:18 +0600

    scroll to end at AppImagePickerList

--- app/components/Form/AppImageInputList.jsx
+++ app/components/Form/AppImageInputList.jsx
@@ -1,22 +1,30 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import AppText from "../AppText";
+import React, { useRef } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+
 import AppImageInput from "./AppImageInput";
 
 const ImageInputList = ({ imageUris, onAddImage, onDeleteImage }) => {
-  return (
-    <View style={styles.container}>
-      {imageUris.map((uri) => (
-        <View style={styles.image} key={uri}>
-          <AppImageInput
-            onChangeImage={() => onDeleteImage(uri)}
-            imageUri={uri}
-          />
-        </View>
-      ))}
+  const scrollViewRef = useRef();
 
-      <AppImageInput onChangeImage={onAddImage} />
-    </View>
+  return (
+    <ScrollView
+      horizontal
+      ref={scrollViewRef}
+      onContentSizeChange={() => scrollViewRef.current.scrollToEnd()}
+    >
+      <View style={styles.container}>
+        {imageUris.map((uri) => (
+          <View style={styles.image} key={uri}>
+            <AppImageInput
+              onChangeImage={() => onDeleteImage(uri)}
+              imageUri={uri}
+            />
+          </View>
+        ))}
+
+        <AppImageInput onChangeImage={onAddImage} />
+      </View>
+    </ScrollView>
   );
 };
 
